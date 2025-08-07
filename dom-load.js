b44b923f2a84a4fa78f298e17a6a981b2da6f183
SUPRIA <supriajaya02@gmail.com>
2025-08-07 08:12:49 +0700

    Update dom-load.js

--- dom-load.js
+++ dom-load.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-
   const firebaseConfig = {
     apiKey: "AIzaSyDE17I5nEIdrfQhfRP5ewloydX18Sw47ws",
     authDomain: "nasukachat.firebaseapp.com",
@@ -8,10 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
     projectId: "nasukachat",
     storageBucket: "nasukachat.firebasestorage.app",
     messagingSenderId: "759589458121",
-    appId: "1:759589458121:web:d5670da0d080017e2ffd18"      
+    appId: "1:759589458121:web:d5670da0d080017e2ffd18"
   };
   if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
   }
   const db = firebase.database();
-}
+});
