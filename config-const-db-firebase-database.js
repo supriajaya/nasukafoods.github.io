da6e889cd510f7883587ba4b6ebebbbdc33e939f
SUPRIA <supriajaya02@gmail.com>
2025-08-18 03:54:00 +0700

    Create config-const-db-firebase-database.js

---
+++ config-const-db-firebase-database.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPJfJgUg8a_e1zS3nSbU8RqHj3TOALX2s",
+    authDomain: "nasuka-fc780.firebaseapp.com",
+    databaseURL: "https://nasuka-fc780-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nasuka-fc780",
+    messagingSenderId: "860641747257",
+    appId: "1:860641747257:web:d1dc28bf34cc1f64ad48e8"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
