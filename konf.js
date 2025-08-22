c682c384180725213fdf71fd39318dddacd62b49
SUPRIA <supriajaya02@gmail.com>
2025-08-22 13:32:45 +0700

    Create konf.js

---
+++ konf.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASa57fEz1c31GwQ_1SR0UyLPbWOEuk8vs",
+  authDomain: "nasukaapp.firebaseapp.com",
+  databaseURL: "https://nasukaapp-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nasukaapp",
+  messagingSenderId: "853986671478",
+  appId: "1:853986671478:web:25b1c3f0b72149c5885857"
+};
