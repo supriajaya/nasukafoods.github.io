c1fd2e28fd9ea3c46ccbcb94297eda6366350a04
SUPRIA <supriajaya02@gmail.com>
2025-08-22 13:36:43 +0700

    Create kompig.js

---
+++ kompig.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyASa57fEz1c31GwQ_1SR0UyLPbWOEuk8vs",
+    authDomain: "nasukaapp.firebaseapp.com",
+    databaseURL: "https://nasukaapp-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nasukaapp",
+    storageBucket: "nasukaapp.firebasestorage.app",
+    messagingSenderId: "853986671478",
+    appId: "1:853986671478:web:25b1c3f0b72149c5885857"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
