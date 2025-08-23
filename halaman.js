422db8e84e024004a1898848d2fa7403cb1826b7
SUPRIA <supriajaya02@gmail.com>
2025-08-23 10:17:24 +0700

    Update halaman.js

--- halaman.js
+++ halaman.js
@@ -1,15 +1,12 @@
-function tampilkanHalaman(halamanId) {
-    const halamans = ['home', 'profil', 'saya', 'inbox', 'belanja', 'beliPerak', 'payment', 'editProfil', 'roll', 'leaderboard', 'faq', 'term', 'burung', 'return', 'privacy', 'faq', 'rule', 'labirin', 'disclaimer'];
-    
-    halamans.forEach(id => {
-        const element = document.getElementById(id + '-halaman');
-        if (element) {
-            element.style.display = 'none';
-        }
-    });
+function showHalaman(halamanId) {
+  const halamans = ['home','profil','editprofil','saya','belanja','payment','burung','roll','labirin','puzzle','rule','disclaimer','leaderboard','faq','return','privacy','term','pasar','zona1','zona2']; 
 
-    const selectedHalaman = document.getElementById(halamanId + '-halaman');
-    if (selectedHalaman) {
-        selectedHalaman.style.display = 'block';
-    }
+  halamans.forEach(id => {
+    const element = document.getElementById(id + '-halaman');
+    if (element) element.style.display = 'none';
+  });
+
+  const selectedHalaman = document.getElementById(halamanId + '-halaman');
+  if (selectedHalaman) selectedHalaman.style.display = 'block';
 }
+  
